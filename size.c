2df598d363a96b9799ba3928aa38ef8541c07a8c
care <cdeniau@student.42.fr>
2015-07-26 21:05:29 +0200

    sample commit

---
+++ size.c
@@ -0,0 +1,5 @@
+
+size_t          get_tiny_size(void)
+{
+    return ((size_t)getpagesize());
+}
---
+++ test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/*
+ * Vous pouvez avoir une variable globale pour gérer vos allocations et une pour le
+ * thread-safe.
+ */
+
+int         main(int ac, char **av)
+{
+    int     i;
+    int     nb;
+    char    c;
+    char    *str;
+
+    (void)ac;
+    i = 0;
+    nb = atoi(av[2]);
+    c = av[1][0];
+    str = (char *)mmap(0, nb + 1, PROT_READ | PROT_WRITE,
+            MAP_ANON | MAP_PRIVATE, -1, 0);
+
+    while (i < nb)
+    {
+        str[i] = c;
+        i++;
+    }
+        str[i] = '\0';
+    printf("%s\n", str);
+    printf("%d\n", getpagesize()); // ce truc renvoie 4096
+    return (0);
+}
