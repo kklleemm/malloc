5661dac4232e82e14de9e16fe72c313c5047caa5
cdeniau <klem.deniau@gmail.com>
2015-08-09 19:05:24 +0200

    dup2

---
+++ libft/ft_strdup2.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdeniau <cdeniau@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/09 17:55:43 by cdeniau           #+#    #+#             */
+/*   Updated: 2015/08/09 18:01:53 by cdeniau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (s1 == NULL)
+		return (NULL);
+	while (s1[i])
+		i++;
+	if (!(str = (char *)ft_malloc(sizeof(*str) * i + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
