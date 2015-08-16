40c43c02a2f511cb7920d5cd54f0284ef6733830
cdeniau <klem.deniau@gmail.com>
2015-08-16 14:52:03 +0200

    large OK

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdeniau <cdeniau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/05 10:01:19 by cdeniau           #+#    #+#             */
-/*   Updated: 2015/08/16 14:32:28 by cdeniau          ###   ########.fr       */
+/*   Updated: 2015/08/16 14:46:24 by cdeniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,6 @@ int		main_realloc(void)
 	return (0);
 }
 
-char	*large_ret(int nb, int size)
-{
-	char	*ret;
-
-	(void)nb;
-	(void)size;
-	ret = NULL;
-	return (ret);
-}
-
 int		main(int ac, char **av)
 {
 	char	*sampletext;
@@ -40,20 +30,24 @@ int		main(int ac, char **av)
 
 	if (!(ft_strcmp(av[1], "R")))
 		return(main_realloc());
-	if (ac < 3 || ac > 4)
-		ft_putendl("Usage : ./test T|S|L|R nbmalloc [size]");
 	i = -1;
-	if (ft_strcmp(av[1], "T") == 0)
+	if (ac == 3)
+	{
+		if (ft_strcmp(av[1], "T") == 0)
 		while (++i < ft_atoi(av[2]))
 			sampletext = malloc(10);
 	else if (ft_strcmp(av[1], "S") == 0)
 		while (++i < ft_atoi(av[2]))
 			sampletext = malloc(1000);
-	else if (ft_strcmp(av[1], "L") == 0)
-	{
-		while (++i < ft_atoi(av[2]))
-			sampletext = malloc(ft_atoi(av[3]));
 	}
+	else if (ac == 4 && !ft_strcmp(av[1], "L"))
+	{
+		if (ft_atoi(av[3]) > 4096)
+			while (++i < ft_atoi(av[2]))
+				sampletext = malloc(ft_atoi(av[3]));
+	}
+	else
+		ft_putendl("Usage : ./test T|S|L|R nbmalloc [size]");
 	ft_print_tiny();
 	ft_print_small();
 	ft_print_large();
--- sources/large.c
+++ sources/large.c
@@ -6,7 +6,7 @@
 /*   By: cdeniau <cdeniau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/13 22:23:32 by cdeniau           #+#    #+#             */
-/*   Updated: 2015/08/16 12:41:53 by cdeniau          ###   ########.fr       */
+/*   Updated: 2015/08/16 14:50:09 by cdeniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,21 @@
 
 void				ft_print_large(void)
 {
-	t_tiny			*large;
+	t_large			*large;
 
 	if (!g_page.large_head)
 		return ;
 	large = (void *)g_page.large_head;
-	ft_putstr("LARGE : ");
 	while (large)
 	{
-		ft_atoi_hex_nl(large->firstblock);
-		while (get_mem_size(large->firstblock))
-		{
-			if (check_flag(large->firstblock) == 1337)
-			{
-				ft_atoi_hex((void *)(large->firstblock + 16));
-				ft_putstr(" - ");
-				ft_atoi_hex((void *)(large->firstblock + large->size));
-				ft_putstr(" : ");
-				ft_putnbr((int)(get_mem_size(large->firstblock)));
-				ft_putstr(" octets        ");
-				print_mem((void *)(large->firstblock + 16));
-			}
-			large->firstblock = *large->firstblock;
-		}
+		ft_putstr("LARGE : ");
+		ft_atoi_hex((void *)(large->page + 16));
+		ft_putstr(" - ");
+		ft_atoi_hex((void *)(large->page + large->size));
+		ft_putstr(" : ");
+		ft_putnbr((int)(large->size));
+		ft_putstr(" octets        ");
+		print_mem((void *)(large->page + 16));
 		large = large->next;
 	}
 }
@@ -80,6 +72,7 @@ t_large				*ft_new_large(size_t size)
 
 	new = mmap(0, sizeof (t_large) + 1, FLAGS, -1, 0);
 	new->page = mmap(0, size * 16, FLAGS, -1, 0);
+	new->size = (int)size;
 	new->next = NULL;
 	return (new);
 }
