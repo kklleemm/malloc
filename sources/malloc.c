e57c3e9a809dbd3228622f44f9bf16477ec1f147
Clement DENIAU <cdeniau@e2r10p20.42.fr>
2015-09-17 14:01:39 +0200

    removing casts, useless structures, and adding tiny+small method

--- sources/malloc.c
+++ sources/malloc.c
@@ -6,7 +6,7 @@
 /*   By: cdeniau <cdeniau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/08 18:14:08 by cdeniau           #+#    #+#             */
-/*   Updated: 2015/09/10 18:21:13 by cdeniau          ###   ########.fr       */
+/*   Updated: 2015/09/17 14:00:08 by cdeniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,48 @@ void				show_alloc_mem(void)
 	ft_print_large();
 }
 
+t_page				*find_page(size_t size)
+{
+	char			type;
+
+	type = get_type(size);
+	page = g_env.page;
+	while (page)
+	{
+		if (page->type == type && !page->full)
+		{
+			if ((page->size + size + 32) > get_max_size(type))
+			{
+				page->full = 1;
+				return (NULL);
+			}
+			else
+			{
+				page->size += size + 32;
+				return (page);
+			}
+		}
+		page = page->next;
+	}
+	return (NULL);
+}
+
 void				*malloc(size_t size)
 {
-	void			*ret;
 	struct rlimit	rlp;
+	t_page			*page;
 
-	ret = NULL;
+	page = NULL;
 	if (getrlimit(RLIMIT_AS, &rlp) < 0)
 		return (NULL);
-	if (size <= TINY)
-		ret = ft_malloc_tiny(size);
-	else if (size <= SMALL)
-		ret = ft_malloc_small(size);
-	else
+	if (rlp.rlim_cur < size)
+		return (NULL); // perror
+	if (size > SMALL)
 		ret = ft_malloc_large(size);
-	return (ret);
+	else if ((page = find_page(size)) != NULL)
+		return (ft_new_malloc(page, get_type(size)));
+	page = get_malloc(page, size);
+	//
+	//
+	return (page);
 }
--- sources/tiny.c
+++ sources/tiny.c
@@ -6,7 +6,7 @@
 /*   By: cdeniau <cdeniau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/16 12:01:56 by cdeniau           #+#    #+#             */
-/*   Updated: 2015/09/10 19:04:45 by cdeniau          ###   ########.fr       */
+/*   Updated: 2015/09/17 13:48:06 by cdeniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,28 @@ void				ft_print_tiny(void)
 	}
 }
 
-void				*ft_malloc_tiny(size_t size)
+char				get_type(size_t size)
+{
+	if (size < TINY)
+		return ('T');
+	else if (size < SMALL)
+		return ('S');
+	else
+		return ('L');
+}
+
+void				*get_malloc(t_page *page, size_t size)
 {
 	void			*ret;
-	t_tiny			*page;
-	int				first;
+	t_page			*cpy;
+	char			type;
 
+	type = get_type;
 	ret = NULL;
-	page = NULL;
-	first = 0;
+	cpy = NULL;
 	if (!g_page.tiny_head)
-	{
-		g_page.tiny_head = ft_new_tiny(size);
-		first = 1;
-	}
-	page = ft_tiny_find(g_page.tiny_head);
+		g_page.tiny_head = ft_new_malloc(size, type); // init ???
+	page = ft_find(g_page.tiny_head);
 	if (page->totalsize + size + 16 < (TINY_PAGE - 32))
 		page->totalsize += size + 16;
 	else
@@ -63,12 +70,12 @@ void				*ft_malloc_tiny(size_t size)
 		first = 1;
 	}
 	ret = set_header(page->firstblock, size, first);
-	return (ret);
+	return (page);
 }
 
-t_tiny				*ft_tiny_find(t_tiny *page)
+t_page				*ft_find(t_tiny *page)
 {
-	t_tiny			*cpy;
+	t_page			*cpy;
 
 	cpy = page;
 	while (cpy->next)
@@ -76,9 +83,9 @@ t_tiny				*ft_tiny_find(t_tiny *page)
 	return (cpy);
 }
 
-t_tiny				*ft_new_tiny(size_t size)
+t_page				*ft_new_malloc(size_t size)
 {
-	t_tiny	*new;
+	t_page			*new;
 
 	if ((new = mmap(0, sizeof(t_tiny) + 1, FLAGS, -1, 0)) == MAP_FAILED)
 		return (ft_overninethousand());
